407e8a95606183043ac9966730f2f21bc17aa9bf
Martin Tange <mktange@gmail.com>
2013-04-08 23:47:32 +0200

    Linear regression 10fold

--- assignment2/code/linear_regression.m
+++ assignment2/code/linear_regression.m
@@ -2,15 +2,11 @@
 clear all;
 close all;
 clc;
-datapath = '../../MNIST Dataset/';
-addpath(['../../Toolbox/MBox']);
-addpath(['../../Toolbox/']);
-addpath(datapath);
+addpath('../../Toolbox/MBox');
+addpath('../../Toolbox/');
+addpath('../../MNIST Dataset/');
 
-load data_cache
-
-Labels = loadMNISTLabels( ...
-    [datapath 'train-labels-idx1-ubyte/train-labels.idx1-ubyte'] );
+load data_cache_2
 
 % This anonymous function takes as input a training and a test set. 
 %  1. It fits a generalized linear model on the training set using glmfit.
@@ -20,40 +16,29 @@ funLinreg = @(X_train, y_train, X_test, y_test) ...
     sum((y_test-glmval(glmfit(X_train, y_train), ...
     X_test, 'identity')).^2);
 
-clear ims nrows
-
 %% Pick a number and attribute to predict
 num = 4;
 attr = 50;
 
+% range = 1:56; % vert+hori
+% range = 57:128; % radi
+range = 129:200; % in-out
+% range = 201:272; % out-in
+
 % Extract only data matching the picked number
-% X = Data(ismember(Labels, num),1:56); % Vert+hori
-% X = Data(ismember(Labels, num),57:128); % radials
-X = Data(ismember(Labels, num),129:200); % In-out
-% X = Data(ismember(Labels, num),201:272); % Out-in
-
-% Combine attributes
-% [N,M] = size(X);
-% c = 4;
-% combinedData = nan(N, M/c);
-% for i=1:M/c,
-%     range = 1+(i-1)*c:i*c;
-%     combinedData(:,i) = sum(X(:,range),2);
-% end;
-% X = combinedData;
-
+X = X_train(ismember(y_train, num), range); 
+attributeNames = attributeNames(range);
 
 % Extract the attribute we want to predict
 y = X(:,attr);
 X(:,attr) = [];
+attributeNames(attr) = [];
 [N,M] = size(X);
 
-attributeNames = 1:length(X(1,:));
-
-clear Labels num attr i c range
+clear num attr
 
 %% Setup cross-validation
-K = 3;
+K = 10;
 CV = cvpartition(N, 'Kfold', K);
 
 % Initialize variables
@@ -76,10 +61,12 @@ for k = 1:K
     y_test = y(CV.test(k));
     
     % Use 10-fold crossvalidation for sequential feature selection
+    tic
     [F, H] = sequentialfs(funLinreg, X_train, y_train);
+    toc
     
     % Save the selected features
-    Features(k,:) = F;    
+    Features(k,:) = F;
     
     % Compute squared error without using the input data at all
     Error_train_nofeatures(k) = sum((y_train-mean(y_train)).^2);
@@ -105,9 +92,21 @@ fprintf('- Test error:     %8.2f\n', sum(Error_test_fs)/sum(CV.TestSize));
 fprintf('- R^2 train:     %8.2f\n', (sum(Error_train_nofeatures)-sum(Error_train_fs))/sum(Error_train_nofeatures));
 fprintf('- R^2 test:     %8.2f\n', (sum(Error_test_nofeatures)-sum(Error_test_fs))/sum(Error_test_nofeatures));
 
-% Show the selected features
+%% Show the selected features
 mfig('Attributes'); clf;
-bmplot(attributeNames, 1:K, Features');
-xlabel('Crossvalidation fold');
-ylabel('Attribute');
+bmplot(1:K, attributeNames, Features);
+xlabel('Attribute');
+ylabel('Crossvalidation fold');
 title('Attributes selected');
+
+set(gca, 'XTick', 1, 'XTickLabel', '');
+hx = get(gca,'XLabel'); % Handle to xlabel 
+set(hx,'Units','data'); 
+pos = get(hx,'Position'); 
+y = pos(2);
+
+% Place the new labels 
+for i = 1:length(attributeNames),
+    t(i) = text(i,y,attributeNames(i)); 
+end;
+set(t,'Rotation',90,'HorizontalAlignment','right') ;
